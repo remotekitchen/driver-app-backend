01618a8319127d25a22a8cc280e7a4110f1643ed
Shahriar-Turag <shahriarturag.st@gmail.com>
2025-02-16 19:54:59 +0600

    test deployment

--- apps/accounts/api/base/views.py
+++ apps/accounts/api/base/views.py
@@ -30,7 +30,6 @@ from apps.accounts.api.v1.serializers import UserSerializer, ProfileSerializer, 
 from apps.accounts.models import User, Profile, Vehicle
 from django.shortcuts import get_object_or_404
 
-# test commit
 class AbstractBaseLoginView(GenericAPIView):
     authentication_classes = []
 
